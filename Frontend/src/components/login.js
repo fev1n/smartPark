5765279ed3fb4e6b734e0644492f19dd18cf5227
Fevin Patel <fevin@fevins-air.home>
2023-09-26 22:12:51 -0400

    Implemented API to make frontend work with backend

--- Frontend/src/components/login.js
+++ Frontend/src/components/login.js
@@ -1,73 +1,102 @@
-import React from 'react';
-import bgpng from '../assets/img2.jpg';
-import { useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import React from "react";
+import bgImg from "../assets/img2.jpg";
+import "../styles/login.css";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import classNames from "classnames";
 export default function Form() {
-  const { register, handleSubmit, formState: { errors, isValid }} = useForm({
-    mode: 'onBlur',
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: "onBlur",
   });
 
-  
   const navigate = useNavigate();
 
-  const onSubmit = (data) => {
-    if (isValid) {
-     
-        console.log(data);
-        navigate('/dashboard', { state: { email: data.email } });
+  async function onSubmit(data) {
+    console.log(data);
+    try {
+      const response = await fetch("http://localhost:4000/api/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer" + localStorage.getItem("token"),
+        },
 
-    } else {
-      console.error('Form contains validation errors.');
+        body: JSON.stringify(data),
+      });
+
+      const responseData = await response.json();
+
+      if (response.status === 200 && responseData.success) {
+        console.log("Login successful!");
+
+        console.log("JWT Token: ", responseData.token);
+
+        navigate("/dashboard", { state: { email: data.email } });
+      } else {
+        console.error("Failed to login:", responseData);
+      }
+    } catch (error) {
+      console.error("Error while logging in:", error);
     }
-  };
+  }
 
   const validateEmail = (email) => {
-    
     const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
     return emailRegex.test(email);
   };
 
+  // Use classNames to conditionally apply classes
+  const col1Classes = classNames("col-1", {
+    "responsive-col-1": window.innerWidth <= 768, // Example condition
+  });
+  const col2Classes = classNames("col-2", {
+    "responsive-col-2": window.innerWidth <= 768, // Example condition
+  });
 
   return (
     <section>
-      <div className="register1">
-        <div className="col-1">
+      <div className="login">
+        <div className={col1Classes}>
           <h2>Smart Park</h2>
-          <span>login and enjoy the service</span>
+          <span>Login and enjoy the service</span>
 
           <form
-            id='form'
-            className='flex flex-col'
+            id="form"
+            className="flex flex-col"
             onSubmit={handleSubmit(onSubmit)}
           >
             <input
               type="text"
               {...register("email", {
                 required: true,
-                validate: validateEmail, 
+                validate: validateEmail,
               })}
-              placeholder='email'
+              placeholder="email"
             />
             {errors.email?.type === "required" && "Email is required"}
             {errors.email?.type === "validate" && "Invalid email format"}
-            
+
             <input
               type="password"
-              {...register('password')}
-              placeholder="password"
+              {...register("password")}
+              placeholder="Password"
             />
-    
-            {errors.confirmpwd?.type === 'validate' &&
-              'Passwords do not match'}
-            <button className='btn' disabled={!isValid}>Login</button>
+
+            {errors.password2?.type === "validate" && "Passwords do not match"}
+            <button className="btn">Login</button>
             <p>
-      Don't have an account? <Link to="/signup">Click here to sign up</Link>.
-    </p>
+              Don't have an account?{" "}
+              <Link to="/signup">Click here to sign up</Link>.
+            </p>
           </form>
         </div>
-        <div className="col-2">
-          <img src={bgpng} alt="" />
+        <div className={col2Classes}>
+          <img src={bgImg} alt="" />
         </div>
       </div>
     </section>
--- Frontend/src/components/signup.js
+++ Frontend/src/components/signup.js
@@ -1,80 +1,108 @@
-import React from 'react';
-import bgImg from '../assets/img1.jpg';
-import { useForm } from 'react-hook-form';
-
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import bgImg from "../assets/img1.jpeg";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import classNames from "classnames";
+import "../styles/signup.css";
 export default function Form() {
-  const { register, handleSubmit, formState: { errors, isValid }, watch } = useForm({
-    mode: 'onBlur',
-    
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+    watch,
+  } = useForm({
+    mode: "onBlur",
   });
   const navigate = useNavigate();
-  const password = watch("password"); 
-  
-  const onSubmit = data => {
+  const password = watch("password");
+
+  async function onSubmit(data) {
     if (isValid) {
-      if (data.password === data.confirmpwd) {
+      if (data.password === data.password2) {
         console.log(data);
-        
-        navigate('/login');
+        await fetch("http://localhost:4000/api/users/register", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }).catch((err) => console.log(err));
+
+        navigate("/login");
       } else {
-        console.error('Passwords do not match.');
+        console.error("Passwords do not match.");
       }
     } else {
-      console.error('Form contains validation errors.');
+      console.error("Form contains validation errors.");
     }
-  };
+  }
 
   const validateEmail = (email) => {
-    
     const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
     return emailRegex.test(email);
   };
 
-  
+  // Use classNames to conditionally apply classes
+  const col1Classes = classNames("col-1", {
+    "responsive-col-1": window.innerWidth <= 768, // Example condition
+  });
+  const col2Classes = classNames("col-2", {
+    "responsive-col-2": window.innerWidth <= 768, // Example condition
+  });
+
   return (
     <section>
       <div className="register">
-        <div className="col-1">
+        <div className={col1Classes}>
+          {" "}
           <h2>Smart Park</h2>
           <span>Register and enjoy the service</span>
-
           <form
-            id='form'
-            className='flex flex-col'
+            id="form"
+            className="flex flex-col"
             onSubmit={handleSubmit(onSubmit)}
           >
             <input
               type="text"
+              {...register("name", {
+                required: true,
+              })}
+              placeholder="Name"
+            />
+            {errors.name?.type === "required" && "Name is required"}
+
+            <input
+              type="text"
               {...register("email", {
                 required: true,
-                validate: validateEmail, 
+                validate: validateEmail,
               })}
-              placeholder='email'
+              placeholder="Email"
             />
             {errors.email?.type === "required" && "Email is required"}
             {errors.email?.type === "validate" && "Invalid email format"}
-            
+
             <input
               type="password"
               {...register("password")}
-              placeholder='password'
+              placeholder="Password"
             />
             <input
               type="password"
-              {...register("confirmpwd", {
-                validate: (value) => value === password 
+              {...register("password2", {
+                validate: (value) => value === password,
               })}
-              placeholder='confirm password'
+              placeholder="Confirm Password"
             />
-            {errors.confirmpwd?.type === "validate" && "Passwords do not match"}
-            
-           
-            <button className='btn' disabled={!isValid}>Sign In</button>
+            {errors.password2?.type === "validate" && "Passwords do not match"}
+
+            <button className="btn" disabled={!isValid}>
+              Sign In
+            </button>
           </form>
         </div>
-        <div className="col-2">
-          <img  src={bgImg} alt="" />
+        <div className={col2Classes}>
+          <img src={bgImg} alt="" />
         </div>
       </div>
     </section>
