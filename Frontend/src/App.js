014001a4a45dbdf39c79fae2d38668a5c13d362a
Fevin Patel <fevin@Fevins-MacBook-Air.local>
2023-10-18 22:40:30 -0400

    Made initial changes to search and map for better results

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -8,16 +7,6 @@ import Dashboard from "./components/dashboard";
 import SearchPage from "./components/search/searchPage.js";
 import SignUpForm from "./components/signup";
 
-=======
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Sign from './components/signup';
-import LoginForm from './components/login';
-import Dashboard from './components/dashboard';
-
-import SignUpForm from './components/signup';
->>>>>>> bfe6a43d52d9857f4014ccb9c0ab5bffb07cc5c2
 function App() {
   return (
     <Router>
@@ -26,11 +15,7 @@ function App() {
         <Route path="/login" element={<LoginForm />} />
         <Route path="/signup" element={<Sign />} />
         <Route path="/dashboard" element={<Dashboard />} />
-<<<<<<< HEAD
         <Route path="/search" element={<SearchPage />} />
-=======
-
->>>>>>> bfe6a43d52d9857f4014ccb9c0ab5bffb07cc5c2
       </Routes>
     </Router>
   );
